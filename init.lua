14bc0db45e6d5c97c120a17347330107e92f021a
Danil P <slemonide@gmail.com>
2017-11-11 19:06:12 -0800

    Added random walk mode parameter. Allows changing probability distribution function for arbitrary intervals of random walk domain.
    Also fixed code formatting (4 spaces).

--- init.lua
+++ init.lua
@@ -5,38 +5,63 @@ local MAX_Z = 30000
 
 
 function generateRandomWalkVector(min, max)
-	local currentHeight = 0
+    local currentHeight = 0
     local val = {}
 
-	for i=0, max do
-		val[i] = currentHeight
-		currentHeight = currentHeight + math.random(-1,1)
-    end
+    local mode = math.random(4); -- mode of random walking, shifts probability distribution
+    local modeAge = 10; -- mode age, when goes to zero, mode is reset
 
+    for i=0, max do
+        val[i] = currentHeight
+        if (mode == 1) then
+            currentHeight = currentHeight + math.random(-1,1)
+        elseif (mode == 2) then
+            if (math.random(3) == 1) then
+                currentHeight = currentHeight + math.random(-1,1)
+            else
+                currentHeight = currentHeight
+            end
+        elseif (mode == 3) then
+            if (math.random(3) == 1) then
+                currentHeight = currentHeight + math.random(-1,1)
+            else
+                currentHeight = currentHeight + 1
+            end
+        elseif (mode == 4) then
+            if (math.random(3) == 1) then
+                currentHeight = currentHeight + math.random(-1,1)
+            else
+                currentHeight = currentHeight - 1
+            end
+        end
+        modeAge = modeAge - 1;
+        if (modeAge == 0) then
+            mode = math.random(4)
+            modeAge = 100
+        end
+    end
     currentHeight = 0
     for i=0, min,-1 do
-		val[i] = currentHeight
-		currentHeight = currentHeight + math.random(-1,1)
+        val[i] = currentHeight
+        currentHeight = currentHeight + math.random(-1,1)
     end
 
     return val
 end
 
 minetest.register_on_mapgen_init(function(mapgen_params)
-	xWalk = generateRandomWalkVector(MIN_X, MAX_X);
-	zWalk = generateRandomWalkVector(MIN_Z, MAX_Z);
-	xzWalk1 = generateRandomWalkVector(MIN_X, MAX_X);
-	xzWalk2 = generateRandomWalkVector(MIN_Z, MAX_Z);
+    xWalk = generateRandomWalkVector(MIN_X, MAX_X);
+    zWalk = generateRandomWalkVector(MIN_Z, MAX_Z);
+    xzWalk1 = generateRandomWalkVector(MIN_X, MAX_X);
+    xzWalk2 = generateRandomWalkVector(MIN_Z, MAX_Z);
 end)
 
 
- minetest.set_mapgen_params({mgname="singlenode"})
+minetest.set_mapgen_params({mgname="singlenode"})
 
- minetest.register_on_generated(function(minp, maxp, seed)
+minetest.register_on_generated(function(minp, maxp, seed)
     local c_stone = minetest.get_content_id("default:stone")
     local c_water = minetest.get_content_id("default:water_source")
-    local c_air = minetest.get_content_id("air")
-    local c_ignore = minetest.get_content_id("ignore")
 
     local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
     local data = vm:get_data()
@@ -47,30 +72,29 @@ end)
     local index2d = 0
     for z = minp.z, maxp.z do
     for y = minp.y, maxp.y do
-    for x = minp.x, maxp.x do      
+    for x = minp.x, maxp.x do
         if xWalk[x] then
-	        index2d = (z - minp.z) * csize.x + (x - minp.x) + 1   
-	        local ivm = a:index(x, y, z)
+            index2d = (z - minp.z) * csize.x + (x - minp.x) + 1   
+            local ivm = a:index(x, y, z)
 
-	        if y < (xWalk[x] + zWalk[z] + xzWalk1[x + z] + xzWalk2[x - z]) / 3 then
-	           data[ivm] = c_stone
-	           write = true
-	        elseif y < 1 then
-	           data[ivm] = c_water
-	           write = true
-	        end
-	     end
+            if y < (xWalk[x] + zWalk[z] + xzWalk1[x + z] + xzWalk2[x - z]) / 3 then
+                data[ivm] = c_stone
+                write = true
+            elseif y < 1 then
+                data[ivm] = c_water
+                write = true
+            end
+         end
      end
      end
      end
 
-       if write then
-          vm:set_data(data)
-          vm:set_lighting({day = 0, night = 0})
-          vm:calc_lighting()
-          vm:update_liquids()
-          vm:write_to_map()
-       end
-
+    if write then
+        vm:set_data(data)
+        vm:set_lighting({day = 0, night = 0})
+        vm:calc_lighting()
+        vm:update_liquids()
+        vm:write_to_map()
     end
-)
+
+end)
